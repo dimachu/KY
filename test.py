6527539c74fef93b4654fd06843be6c05f067aad
Dmitry <dima.chu.00@mail.ru>
2024-12-22 14:41:51 +0300

    main

--- test.py
+++ test.py
@@ -1,34 +1,34 @@
 import unittest
-from viz_deps import strip_version  # Убедитесь, что импортируете вашу функцию правильно
+from viz import strip  # Убедитесь, что импортируете вашу функцию правильно
 
 class TestStripVersion(unittest.TestCase):
 
     def test_basic_dependency(self):
-        self.assertEqual(strip_version('requests'), 'requests')
+        self.assertEqual(strip('requests'), 'requests')
 
     def test_simple_version(self):
-        self.assertEqual(strip_version('requests>=2.0.0'), 'requests')
+        self.assertEqual(strip('requests>=2.0.0'), 'requests')
 
     def test_extra(self):
-        self.assertEqual(strip_version('requests[security]'), 'requests')
+        self.assertEqual(strip('requests[security]'), 'requests')
 
     def test_version_with_extra(self):
-        self.assertEqual(strip_version('requests[security]>=2.0.0'), 'requests')
+        self.assertEqual(strip('requests[security]>=2.0.0'), 'requests')
 
     def test_complex_requirements(self):
-        self.assertEqual(strip_version('some-package; os_version == "Linux"'), 'some-package')
+        self.assertEqual(strip('some-package; os_version == "Linux"'), 'some-package')
 
     def test_multiple_conditions(self):
-        self.assertEqual(strip_version('some-package; python_version < "3.8"'), 'some-package')
+        self.assertEqual(strip('some-package; python_version < "3.8"'), 'some-package')
 
     def test_ignore_version_with_conditions(self):
-        self.assertEqual(strip_version('requests<3.0; python_version >= "3.6"'), 'requests')
+        self.assertEqual(strip('requests<3.0; python_version >= "3.6"'), 'requests')
 
     def test_empty_string(self):
-        self.assertEqual(strip_version(''), '')
+        self.assertEqual(strip(''), '')
 
     def test_white_space(self):
-        self.assertEqual(strip_version('   requests   '), 'requests')
+        self.assertEqual(strip('   requests   '), 'requests')
 
 if __name__ == '__main__':
     unittest.main()
